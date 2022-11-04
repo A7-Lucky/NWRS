d49910a2ffe84c91ce0030816d672af7a2cd1dcb
9yuhyeon <kkh2742@gmail.com>
2022-11-04 10:07:32 +0900

    refact : 리뷰 코드 정리
     - ReviewDetailView 삭제
    - WebtoonReviewDetailView 추가(put, delete)
    - WebtoonReviewDetailView path 추가(urls.py)

--- webtoon/urls.py
+++ webtoon/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.WebtoonView.as_view(), name='webtoon_view'),
     path('<int:webtoon_id>/', views.WebtoonDetailView.as_view(), name='Webtoon_detail_view'),
     path('<int:webtoon_id>/review/', views.WebtoonReviewView.as_view(), name='webtoon_review_view'),
+    path('<int:webtoon_id>/review/<int:review_id>/', views.WebtoonReviewDetailView.as_view(), name='webtoon_review_detail_view'),
     path('myreview/', views.MyReviewView.as_view(), name='my_review_view'),
     path('myreview/<int:review_id>/', views.MyReviewDetailView.as_view(), name='my_review_detail_view'),
     path('<int:webtoon_id>/bookmark/', views.BookmarkView.as_view(), name='bookmark_view'),
